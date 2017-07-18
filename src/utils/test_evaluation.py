5ff4ee625beebc06c2f9f8d9e5bc284792b9fcd3
Yana Hasson <yana.hasson@inria.fr>
2017-07-18 13:49:36 +0200

    Fix evaluation tests to match new input types

--- src/utils/test_evaluation.py
+++ src/utils/test_evaluation.py
@@ -6,7 +6,7 @@ from .evaluation import batch_topk_accuracy
 
 def test_top1_1_accuracy():
     gt = torch.eye(4, 10)
-    gt_var = torch.autograd.Variable(gt)
+    gt_var = gt
     assert batch_topk_accuracy(gt_var, gt, k=1) == 1
 
 
@@ -14,7 +14,7 @@ def test_top1_0_4_accuracy():
     n_row = 10
     n_col = 4
     gt = torch.eye(n_row, n_col)
-    gt_var = torch.autograd.Variable(gt)
+    gt_var = gt
     topk_score = batch_topk_accuracy(gt_var, gt, k=1)
     assert topk_score == n_col / n_row
 
@@ -24,7 +24,7 @@ def test_top1_custom_accuracy():
                           [1.2, 0.4, 1.5, -0.1, 0]])
     np_gt = np.array([[0, 1, 0, 0, 0],
                       [0, 0, 1, 0, 0]])
-    var_scores = torch.autograd.Variable(torch.from_numpy(np_scores))
+    var_scores = torch.from_numpy(np_scores)
     t_gt = torch.from_numpy(np_gt)
 
     top1_score = batch_topk_accuracy(var_scores, t_gt, k=1)
@@ -36,7 +36,7 @@ def test_top2_custom_accuracy():
                           [1.2, 0.4, 1.5, -0.1, 0]])
     np_gt = np.array([[0, 0, 1, 0, 0],
                       [0, 0, 1, 0, 0]])
-    var_scores = torch.autograd.Variable(torch.from_numpy(np_scores))
+    var_scores = torch.from_numpy(np_scores)
     t_gt = torch.from_numpy(np_gt)
 
     top2_score = batch_topk_accuracy(var_scores, t_gt, k=2)
@@ -48,7 +48,7 @@ def test_top3_custom_accuracy():
                           [1.2, 0.4, 1.5, -0.1, 0]])
     np_gt = np.array([[0, 0, 0, 0, 1],
                       [0, 0, 0, 1, 0]])
-    var_scores = torch.autograd.Variable(torch.from_numpy(np_scores))
+    var_scores = torch.from_numpy(np_scores)
     t_gt = torch.from_numpy(np_gt)
 
     top3_score = batch_topk_accuracy(var_scores, t_gt, k=3)
@@ -64,7 +64,7 @@ def test_top5_custom_accuracy():
                       [0, 0, 0, 1, 0, 0, 0],  # Wrong
                       [0, 0, 0, 1, 0, 0, 0],  # Right
                       [0, 0, 0, 0, 0, 0, 1]])  # Wrong
-    var_scores = torch.autograd.Variable(torch.from_numpy(np_scores))
+    var_scores = torch.from_numpy(np_scores)
     t_gt = torch.from_numpy(np_gt)
 
     top5_score = batch_topk_accuracy(var_scores, t_gt, k=5)
