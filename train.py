e8a91191bdb13b51f4d6d1328d33a069037413d2
Yana Hasson <yana.hasson@inria.fr>
2017-08-10 10:15:17 +0200

    train.py script exports run_training function and can still be run itself as before

--- train.py
+++ train.py
@@ -6,125 +6,129 @@ import torchvision.models as models
 from src.datasets import gtea, gun
 from src.datasets.gteagazeplusimage import GTEAGazePlusImage
 from src.options import train_options, error
-from src.nets import resnet_adapt, netutils
+from src.nets import resnet_adapt
 from src.netscripts import train
 from src.utils.normalize import Unnormalize
 from src.utils import evaluation
 
 
-opt = train_options.TrainOptions().parse()
+def run_training(opt):
+    # Normalize as imageNet
+    img_means = [0.485, 0.456, 0.406]
+    img_stds = [0.229, 0.224, 0.225]
+    normalize = transforms.Normalize(mean=img_means,
+                                     std=img_stds)
 
+    # Compute reverse of normalize transfor
+    unnormalize = Unnormalize(mean=img_means, std=img_stds)
 
-# Normalize as imageNet
-img_means = [0.485, 0.456, 0.406]
-img_stds = [0.229, 0.224, 0.225]
-normalize = transforms.Normalize(mean=img_means,
-                                 std=img_stds)
+    # Set input tranformations
+    transformations = ([
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()
+    ])
 
-# Compute reverse of normalize transfor
-unnormalize = Unnormalize(mean=img_means, std=img_stds)
+    if opt.normalize:
+        transformations.append(normalize)
+        first_transforms = [transforms.Scale(230), transforms.RandomCrop(224)]
+        transformations = first_transforms + transformations
 
-# Set input tranformations
-transformations = ([
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor()
-])
+    transform = transforms.Compose(transformations)
 
-if opt.normalize:
-    transformations.append(normalize)
-    first_transforms = [transforms.Scale(230), transforms.RandomCrop(224)]
-    transformations = first_transforms + transformations
+    # Index of sequence item to leave out for validation
+    leave_out_idx = opt.leave_out
 
-transform = transforms.Compose(transformations)
-
-# Index of sequence item to leave out for validation
-leave_out_idx = opt.leave_out
-
-# Create dataset
-if opt.dataset == 'gtea':
-    seqs = ['S1', 'S2', 'S3', 'S4']
-    train_seqs, valid_seqs = evaluation.leave_one_out(seqs, leave_out_idx)
-
-    dataset = gtea.GTEA(transform=transform, untransform=unnormalize,
-                        seqs=train_seqs, no_action_label=False)
-    valid_dataset = gtea.GTEA(transform=transform,
-                              untransform=unnormalize,
-                              seqs=valid_seqs, no_action_label=False)
-    valid = True
-
-elif opt.dataset == 'gteagazeplus':
     # Create dataset
-    all_subjects = ['Ahmad', 'Alireza', 'Carlos',
-                    'Rahul', 'Yin', 'Shaghayegh']
-    train_seqs, valid_seqs = evaluation.leave_one_out(all_subjects,
-                                                      leave_out_idx)
-    dataset = GTEAGazePlusImage(transform=transform,
-                                untransform=unnormalize,
-                                seqs=train_seqs)
-    valid_dataset = GTEAGazePlusImage(transform=transform,
-                                      untransform=unnormalize,
-                                      seqs=valid_seqs)
-    valid = True
+    if opt.dataset == 'gtea':
+        seqs = ['S1', 'S2', 'S3', 'S4']
+        train_seqs, valid_seqs = evaluation.leave_one_out(seqs, leave_out_idx)
 
-elif opt.dataset == 'gun':
-    test_subject_id = 2
-    # Leave one out training
-    seqs = ['Subject1', 'Subject2',
-            'Subject3', 'Subject4',
-            'Subject5', 'Subject6',
-            'Subject7', 'Subject8']
-    train_seqs, valid_seqs = evaluation.leave_one_out(seqs,
-                                                      test_subject_id)
+        dataset = gtea.GTEA(transform=transform, untransform=unnormalize,
+                            seqs=train_seqs, no_action_label=False)
+        valid_dataset = gtea.GTEA(transform=transform,
+                                  untransform=unnormalize,
+                                  seqs=valid_seqs, no_action_label=False)
+        valid = True
 
-    dataset = gun.GUN(transform=transform, untransform=unnormalize,
-                      seqs=train_seqs)
+    elif opt.dataset == 'gteagazeplus':
+        # Create dataset
+        all_subjects = ['Ahmad', 'Alireza', 'Carlos',
+                        'Rahul', 'Yin', 'Shaghayegh']
+        train_seqs, valid_seqs = evaluation.leave_one_out(all_subjects,
+                                                          leave_out_idx)
+        dataset = GTEAGazePlusImage(transform=transform,
+                                    untransform=unnormalize,
+                                    seqs=train_seqs)
+        valid_dataset = GTEAGazePlusImage(transform=transform,
+                                          untransform=unnormalize,
+                                          seqs=valid_seqs)
+        valid = True
 
-    valid_dataset = gun.GUN(transform=transform, untransform=unnormalize,
-                            seqs=valid_seqs)
-    valid = True
+    elif opt.dataset == 'gun':
+        test_subject_id = 2
+        # Leave one out training
+        seqs = ['Subject1', 'Subject2',
+                'Subject3', 'Subject4',
+                'Subject5', 'Subject6',
+                'Subject7', 'Subject8']
+        train_seqs, valid_seqs = evaluation.leave_one_out(seqs,
+                                                          test_subject_id)
 
-print('Dataset size : {0}'.format(len(dataset)))
+        dataset = gun.GUN(transform=transform, untransform=unnormalize,
+                          seqs=train_seqs)
 
-dataloader = torch.utils.data.DataLoader(
-    dataset, shuffle=True, batch_size=opt.batch_size,
-    num_workers=opt.threads, drop_last=True)
+        valid_dataset = gun.GUN(transform=transform, untransform=unnormalize,
+                                seqs=valid_seqs)
+        valid = True
 
-if valid:
-    valid_dataloader = torch.utils.data.DataLoader(valid_dataset,
-                                                   shuffle=False,
-                                                   batch_size=opt.batch_size,
-                                                   num_workers=opt.threads)
+    print('Dataset size : {0}'.format(len(dataset)))
 
-# Load model
-resnet = models.resnet18(pretrained=opt.pretrained)
-model = resnet_adapt.ResNetAdapt(opt, resnet, dataset.class_nb)
+    dataloader = torch.utils.data.DataLoader(
+        dataset, shuffle=True, batch_size=opt.batch_size,
+        num_workers=opt.threads, drop_last=True)
 
-if opt.lr != opt.new_lr:
-    model_params = model.lr_params(lr=opt.new_lr)
-else:
-    model_params = model.net.parameters()
+    if valid:
+        valid_dataloader = torch.utils.data.DataLoader(
+            valid_dataset, shuffle=False,
+            batch_size=opt.batch_size,
+            num_workers=opt.threads,
+            drop_last=True)
 
-optimizer = torch.optim.SGD(model_params, lr=opt.lr,
-                            momentum=opt.momentum)
+    # Load model
+    resnet = models.resnet18(pretrained=opt.pretrained)
+    model = resnet_adapt.ResNetAdapt(opt, resnet, dataset.class_nb)
 
-if opt.criterion == 'MSE':
-    criterion = torch.nn.MSELoss()
-elif opt.criterion == 'CE':
-    criterion = torch.nn.CrossEntropyLoss()
-else:
-    raise error.ArgumentError(
-        '{0} is not among known error functions'.format(opt.criterion))
-
-model.set_criterion(criterion)
-model.set_optimizer(optimizer)
-
-# Load existing weights, opt.continue_training is epoch to load
-if opt.continue_training:
-    if opt.continue_epoch == 0:
-        model.net.eval()
-        model.load('latest')
+    if opt.lr != opt.new_lr:
+        model_params = model.lr_params(lr=opt.new_lr)
     else:
-        model.load(opt.continue_epoch)
+        model_params = model.net.parameters()
 
-train.train_net(dataloader, model, opt,
-                valid_dataloader=valid_dataloader)
+    optimizer = torch.optim.SGD(model_params, lr=opt.lr,
+                                momentum=opt.momentum)
+
+    if opt.criterion == 'MSE':
+        criterion = torch.nn.MSELoss()
+    elif opt.criterion == 'CE':
+        criterion = torch.nn.CrossEntropyLoss()
+    else:
+        raise error.ArgumentError(
+            '{0} is not among known error functions'.format(opt.criterion))
+
+    model.set_criterion(criterion)
+    model.set_optimizer(optimizer)
+
+    # Load existing weights, opt.continue_training is epoch to load
+    if opt.continue_training:
+        if opt.continue_epoch == 0:
+            model.net.eval()
+            model.load('latest')
+        else:
+            model.load(opt.continue_epoch)
+
+    train.train_net(dataloader, model, opt,
+                    valid_dataloader=valid_dataloader)
+
+
+if __name__ == "__main__":
+    opt = train_options.TrainOptions().parse()
+    run_training(opt)
