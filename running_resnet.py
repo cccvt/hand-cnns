e9ce4de0eeaf461cba3a56f4cc2ce809ea087d9c
Yana Hasson <yana.hasson@inria.fr>
2017-08-10 17:21:55 +0200

    Add script for running res_net training function

---
+++ running_resnet.py
@@ -0,0 +1,19 @@
+from train import run_training
+from src.options.train_options import TrainOptions
+
+
+leave_outs = list(range(6))
+
+
+for leave_out in leave_outs:
+    opt = TrainOptions().parse(['--batch_size', '8',
+                                '--leave_out', str(leave_out),
+                                '--lr', '0.0001',
+                                '--new_lr', '0.0001',
+                                '--threads', '1',
+                                '--epochs', '20',
+                                '--exp-id',
+                                'run_leave_outs/gtea_lo_' +
+                                str(leave_out)])
+
+run_training(opt)
