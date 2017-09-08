b3e993714765d4f6921f2231fcaec786417cd637
Yana Hasson <yanahasson@gmail.com>
2017-09-08 15:49:33 +0200

    Propagate option refactoring to run_c3d_smthg

--- running_c3d_smthg.py
+++ running_c3d_smthg.py
@@ -1,19 +1,27 @@
 from c3d_train import run_training
-from src.options.train_options import TrainOptions
+from src.options import base_options, train_options, video_options
 
 
 lrs = [0.0001]
 for lr in lrs:
-    opt = TrainOptions().parse(['--batch_size', '6',
-                                '--dataset', 'smthgsmthg',
-                                '--lr', str(lr),
-                                '--new_lr', str(lr),
-                                '--threads', '3',
-                                '--epochs', '101',
-                                '--exp_id',
-                                'run_c3d_smthg_spacing_3/lr_' +
-                                str(lr),
-                                '--visualize', '0',
-                                '--clip_spacing', '3'])
+    # Initialize base options
+    options = base_options.BaseOptions()
+
+    # Add train options and parse
+    train_options.add_train_options(options)
+    video_options.add_video_options(options)
+
+    opt = options.parse(['--batch_size', '6',
+                         '--dataset', 'smthgsmthg',
+                         '--lr', str(lr),
+                         '--new_lr', str(lr),
+                         '--threads', '3',
+                         '--epochs', '101',
+                         '--exp_id',
+                         'run_c3d_smthg_spacing_3/lr_' +
+                         str(lr),
+                         '--visualize', '0',
+                         '--test_aggreg', '0',
+                         '--clip_spacing', '3'])
 
     run_training(opt)
