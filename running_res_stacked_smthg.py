4381bd762e8ba7eda60a76460bf66d365710206c
Yana Hasson <yanahasson@gmail.com>
2017-09-12 10:10:31 +0200

    Add running script for stacked smthg

---
+++ running_res_stacked_smthg.py
@@ -0,0 +1,30 @@
+from res_stacked_train import run_training
+from src.options import base_options, train_options, video_options, stack_options
+
+
+lrs = [0.0001]
+for lr in lrs:
+    # Initialize base options
+    options = base_options.BaseOptions()
+
+    # Add train options and parse
+    train_options.add_train_options(options)
+    video_options.add_video_options(options)
+    stack_options.add_stack_options(options)
+
+    opt = options.parse(['--batch_size', '8',
+                         '--dataset', 'smthgsmthg',
+                         '--lr', str(lr),
+                         '--new_lr', str(lr),
+                         '--threads', '10',
+                         '--epochs', '101',
+                         '--use_flow', '1',
+                         '--exp_id',
+                         'run_res_stack_smthg_flow_farneback/lr_' +
+                         str(lr),
+                         '--visualize', '0',
+                         '--display_freq', '100',
+                         '--stack_nb', '10',
+                         '--test_aggreg', '0'])
+
+    run_training(opt)
