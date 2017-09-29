38bf9e7fee214df3b371a76b7f90d1f35b0f6f28
Yana Hasson <yanahasson@gmail.com>
2017-09-29 10:43:16 +0200

    Run aggreg running_c3d_smthg on stacked tvl1 flow

--- running_c3d_smthg.py
+++ running_c3d_smthg.py
@@ -1,7 +1,6 @@
 from c3d_train import run_training
 from src.options import base_options, train_options, video_options
 
-
 lrs = [0.0001]
 for lr in lrs:
     # Initialize base options
@@ -11,18 +10,14 @@ for lr in lrs:
     train_options.add_train_options(options)
     video_options.add_video_options(options)
 
-    opt = options.parse(['--batch_size', '6',
-                         '--dataset', 'smthgsmthg',
-                         '--lr', str(lr),
-                         '--new_lr', str(lr),
-                         '--threads', '3',
-                         '--epochs', '101',
-                         '--exp_id',
-                         'run_c3d_smthg_flow/lr_' +
-                         str(lr),
-                         '--visualize', '0',
-                         '--use_flow', '1',
-                         '--test_aggreg', '0',
-                         '--clip_spacing', '1'])
+    opt = options.parse([
+        '--batch_size', '6', '--dataset', 'smthgsmthg', '--lr',
+        str(lr), '--new_lr',
+        str(lr), '--threads', '8', '--epochs', '101', '--exp_id',
+        'train/c3d/flow-tvl1/smthg/run_1_lr_' + str(lr), '--visualize', '1',
+        '--display_freq', '100', '--use_flow', '1', '--flow_type', 'tvl1',
+        '--test_aggreg', '1', '--continue_training', '--continue_epoch', '60',
+        '--clip_spacing', '1'
+    ])
 
     run_training(opt)
