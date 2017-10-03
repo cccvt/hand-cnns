002de9086eda0347d0c58af18b15b654ee5902fa
Yana Hasson <yanahasson@gmail.com>
2017-10-03 19:01:08 +0200

    Fix stack test options (for flow)

--- stack_test.py
+++ stack_test.py
@@ -18,31 +18,42 @@ def run_testing(opt):
         channel_nb = 2
     else:
         channel_nb = 3
-    base_transform_list = [video_transforms.Scale(final_size),
-                           stack_transforms.ToStackedTensor(channel_nb=channel_nb)]
+    base_transform_list = [
+        video_transforms.Scale(final_size),
+        stack_transforms.ToStackedTensor(channel_nb=channel_nb)
+    ]
     base_transform = video_transforms.Compose(base_transform_list)
 
     if opt.dataset == 'smthgsmthg':
-        dataset = SmthgVideo(video_transform=base_transform,
-                             base_transform=base_transform,
-                             clip_size=opt.stack_nb, split=opt.split,
-                             use_flow=opt.use_flow)
+        dataset = SmthgVideo(
+            base_transform=base_transform,
+            clip_size=opt.stack_nb,
+            flow_type=opt.flow_type,
+            rescale_flows=opt.rescale_flows,
+            split=opt.split,
+            use_flow=opt.use_flow,
+            video_transform=base_transform)
     elif opt.dataset == 'gteagazeplus':
-        all_subjects = ['Ahmad', 'Alireza', 'Carlos',
-                        'Rahul', 'Yin', 'Shaghayegh']
-        train_seqs, valid_seqs = evaluation.leave_one_out(all_subjects,
-                                                          opt.leave_out)
-        dataset = GTEAGazePlusVideo(video_transform=base_transform,
-                                    base_transform=base_transform,
-                                    use_video=False,
-                                    clip_size=opt.stack_nb,
-                                    original_labels=True,
-                                    seqs=train_seqs, use_flow=opt.use_flow)
+        all_subjects = [
+            'Ahmad', 'Alireza', 'Carlos', 'Rahul', 'Yin', 'Shaghayegh'
+        ]
+        train_seqs, valid_seqs = evaluation.leave_one_out(
+            all_subjects, opt.leave_out)
+        dataset = GTEAGazePlusVideo(
+            base_transform=base_transform,
+            clip_size=opt.stack_nb,
+            flow_type=opt.flow_type,
+            original_labels=True,
+            rescale_flows=opt.rescale_flows,
+            seqs=train_seqs,
+            use_flow=opt.use_flow,
+            use_video=False,
+            video_transform=base_transform)
 
     # Initialize neural network
     resnet = models.resnet50()
-    model = resnet_adapt.ResNetAdapt(opt, resnet, dataset.class_nb,
-                                     in_channels=channel_nb * opt.stack_nb)
+    model = resnet_adapt.ResNetAdapt(
+        opt, resnet, dataset.class_nb, in_channels=channel_nb * opt.stack_nb)
 
     optimizer = torch.optim.SGD(model.net.parameters(), lr=1)
 
@@ -54,15 +65,20 @@ def run_testing(opt):
         model.net.cuda()
     model.load(load_path=opt.checkpoint_path)
 
-    mean_scores = test.test(dataset, model, opt=opt, frame_nb=opt.frame_nb,
-                            save_predictions=opt.save_predictions)
+    mean_scores = test.test(
+        dataset,
+        model,
+        opt=opt,
+        frame_nb=opt.frame_nb,
+        save_predictions=opt.save_predictions)
     print(mean_scores)
 
     viz = Visualize(opt)
     # Display and save validations info
-    viz.log_errors(epoch=0,
-                   errors={'val_aggr_err': mean_scores},
-                   log_path=viz.valid_aggreg_log_path)
+    viz.log_errors(
+        epoch=0,
+        errors={'val_aggr_err': mean_scores},
+        log_path=viz.valid_aggreg_log_path)
 
 
 if __name__ == '__main__':
