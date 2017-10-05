7b251f073b12c20bef7b64e2fdff6a93637ef755
Yana Hasson <yanahasson@gmail.com>
2017-10-05 16:03:20 +0200

    Params for 5 stacked resnet flow

--- running_res_stacked_smthg.py
+++ running_res_stacked_smthg.py
@@ -9,13 +9,13 @@ train_options.add_train_options(options)
 video_options.add_video_options(options)
 stack_options.add_stack_options(options)
 
-lr = '0.00001'
+lr = '0.0001'
 opt = options.parse([
     '--batch_size', '8', '--dataset', 'smthgsmthg', '--lr', lr, '--new_lr', lr,
     '--threads', '8', '--epochs', '200', '--use_flow', '1', '--flow_type',
-    'tvl1', '--exp_id', 'train/stack/flow-tvl1/smthg/continue_45_lr_' + lr,
-    '--visualize', '1', '--display_freq', '100', '--stack_nb', '10',
-    '--continue_training', '--continue_epoch', '45', '--test_aggreg', '0'
+    'tvl1', '--exp_id', 'train/stack/flow-tvl1/smthg/run_stack_5' + lr,
+    '--visualize', '1', '--display_freq', '100', '--stack_nb', '5',
+    '--test_aggreg', '0'
 ])
 
 run_training(opt)
