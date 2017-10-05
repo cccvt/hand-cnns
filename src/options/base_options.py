ab5410851709625959652559c056851f31d1164a
Yana Hasson <yanahasson@gmail.com>
2017-10-05 10:51:47 +0200

    Refactor option processing

--- src/options/base_options.py
+++ src/options/base_options.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 
 from src.utils import filesys
+from src.utils import options
 
 
 class BaseOptions():
@@ -14,68 +15,79 @@ class BaseOptions():
 
     def initialize(self):
 
-        self.parser.add_argument('--use_gpu', type=int, default=1,
-                                 help='Whether to use gpu (1) or cpu (0)')
+        self.parser.add_argument(
+            '--use_gpu',
+            type=int,
+            default=1,
+            help='Whether to use gpu (1) or cpu (0)')
         # Input params
-        self.parser.add_argument('--dataset', type=str, default='gteagazeplus',
-                                 help='dataset to use among\
+        self.parser.add_argument(
+            '--dataset',
+            type=str,
+            default='gteagazeplus',
+            help='dataset to use among\
                                  (uciego|gtea|gteagazeplus|smthgsmthg)')
-        self.parser.add_argument('--threads', type=int, default=4,
-                                 help='number of threads used for data\
+        self.parser.add_argument(
+            '--threads',
+            type=int,
+            default=4,
+            help='number of threads used for data\
                                  loading')
-        self.parser.add_argument('--use_flow', type=int, default=0,
-                                 help='Whether to use flow or RGB')
-        self.parser.add_argument('--flow_type', type=str, default='tvl1',
-                                 help='in [farn|tvl1]')
-        self.parser.add_argument('--rescale_flows', type=int, default='0',
-                                 help='0 to stay in [0,255], 1 for [min,max]')
-        self.parser.add_argument('--batch_size', type=int, default=2,
-                                 help='input mini-batch size')
+        self.parser.add_argument(
+            '--use_flow',
+            type=int,
+            default=0,
+            help='Whether to use flow or RGB')
+        self.parser.add_argument(
+            '--flow_type', type=str, default='tvl1', help='in [farn|tvl1]')
+        self.parser.add_argument(
+            '--rescale_flows',
+            type=int,
+            default='0',
+            help='0 to stay in [0,255], 1 for [min,max]')
+        self.parser.add_argument(
+            '--batch_size', type=int, default=2, help='input mini-batch size')
         # Save params
-        self.parser.add_argument('--checkpoint_dir', type=str,
-                                 default='./checkpoints',
-                                 help='where to save models')
-        self.parser.add_argument('--exp_id', type=str, default='experiment',
-                                 help='name of experiment, determines where\
+        self.parser.add_argument(
+            '--checkpoint_dir',
+            type=str,
+            default='./checkpoints',
+            help='where to save models')
+        self.parser.add_argument(
+            '--exp_id',
+            type=str,
+            default='experiment',
+            help='name of experiment, determines where\
                                  to store experiment data')
 
         # Averaging params
-        self.parser.add_argument('--frame_nb', type=int,
-                                 default=10, help='number of frames to average\
+        self.parser.add_argument(
+            '--frame_nb',
+            type=int,
+            default=10,
+            help='number of frames to average\
                                  at test time')
 
         # Display params
-        self.parser.add_argument('--visualize', type=int,
-                                 default=1, help='0 to disable visdom plots')
+        self.parser.add_argument(
+            '--visualize',
+            type=int,
+            default=1,
+            help='0 to disable visdom plots')
 
         # GTEAGaze+ specific option
-        self.parser.add_argument('--leave_out', type=int, default=0,
-                                 help="Index of sequence item to leave out\
+        self.parser.add_argument(
+            '--leave_out',
+            type=int,
+            default=0,
+            help="Index of sequence item to leave out\
                                  for validation")
 
     def parse(self, arguments=None):
         self.opt = self.parser.parse_args(arguments)
 
-        # Print  options
-        args = vars(self.opt)
-
-        print('---- Options ----')
-        for k, v in sorted(args.items()):
-            print('{option}: {value}'.format(option=k, value=v))
-
-        # Save options
+        # Print and save options
         exp_dir = os.path.join(self.opt.checkpoint_dir, self.opt.exp_id)
-        filesys.mkdir(exp_dir)
-        opt_path = os.path.join(exp_dir, 'opt.txt')
-        with open(opt_path, 'a') as opt_file:
-            opt_file.write('====== Options ======\n')
-            for k, v in sorted(args.items()):
-                opt_file.write('{option}: {value}\n'.format(
-                    option=str(k), value=str(v)))
-            git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-            opt_file.write('git hash: {}\n'.format(git_hash.strip()))
-            opt_file.write('launched {} at {}\n'.format(
-                str(sys.argv[0]),
-                str(datetime.datetime.now())))
+        options.process_args(self.opt, exp_dir)
 
         return self.opt
---
+++ src/utils/options.py
@@ -0,0 +1,30 @@
+import datetime
+import os
+import subprocess
+import sys
+
+from src.utils import filesys
+
+
+def process_args(args, save_folder=None):
+    # Print  options
+    opts = vars(args)
+
+    print('---- Options ----')
+    for k, v in sorted(opts.items()):
+        print('{option}: {value}'.format(option=k, value=v))
+    print('-----------------\n')
+
+    # Save options
+    if save_folder is not None:
+        filesys.mkdir(save_folder)
+        opt_path = os.path.join(save_folder, 'opt.txt')
+        with open(opt_path, 'a') as opt_file:
+            opt_file.write('====== Options ======\n')
+            for k, v in sorted(opts.items()):
+                opt_file.write(
+                    '{option}: {value}\n'.format(option=str(k), value=str(v)))
+            git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+            opt_file.write('git hash: {}\n'.format(git_hash.strip()))
+            opt_file.write('launched {} at {}\n'.format(
+                str(sys.argv[0]), str(datetime.datetime.now())))
