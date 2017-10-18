6dcd724d52d998ca8f251f13133151fd13554295
Yana Hasson <yana.hasson@inria.fr>
2017-10-18 14:13:22 +0200

    Add new c3d for gteagazeplus

--- running_c3d_gteagazeplus.py
+++ running_c3d_gteagazeplus.py
@@ -1,9 +1,7 @@
 from c3d_train import run_training
 from src.options import base_options, train_options, video_options
 
-
 leave_outs = list(range(0, 6))
-
 
 for leave_out in leave_outs:
     # Initialize base options
@@ -13,17 +11,12 @@ for leave_out in leave_outs:
     train_options.add_train_options(options)
     video_options.add_video_options(options)
 
-    opt = options.parse(['--batch_size', '6',
-                         '--leave_out', str(leave_out),
-                         '--lr', '0.0001',
-                         '--new_lr', '0.0001',
-                         '--threads', '10',
-                         '--epochs', '101',
-                         '--use_flow', '0',
-                         '--exp_id',
-                         'run_c3d_gtea_rgb_frame_leave_outs/gtea_lo_' +
-                         str(leave_out),
-                         '--visualize', '0',
-                         '--test_aggreg', '1'])
+    opt = options.parse([
+        '--batch_size', '6', '--leave_out',
+        str(leave_out), '--lr', '0.0001', '--new_lr', '0.0001', '--threads',
+        '10', '--epochs', '50', '--use_flow', '0', '--exp_id',
+        'train/c3d/rgb/gteagazeplus_fix/leave_outs/gtea_lo_' + str(leave_out),
+        '--visualize', '0', '--test_aggreg', '0'
+    ])
 
     run_training(opt)
