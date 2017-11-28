eb04ebcafeeb122e462d4e9e55ff79087597e0f9
Yana Hasson <yana.hasson@inria.fr>
2017-11-28 14:34:58 +0100

    Fix src/netscripts/train.py indentation

--- src/netscripts/train.py
+++ src/netscripts/train.py
@@ -10,9 +10,12 @@ from src.utils.evaluation import Metric
 from src.netscripts.test import test
 
 
-def train_net(dataloader, model, opt,
+def train_net(dataloader,
+              model,
+              opt,
               valid_dataloader=None,
-              verbose=False, visualize=True,
+              verbose=False,
+              visualize=True,
               test_aggreg=True):
     """
     Args:
@@ -52,11 +55,9 @@ def train_net(dataloader, model, opt,
         start_epoch = 0
 
     # Conf mat are overwritten when training is continued
-    conf_mat = np.zeros((last_epoch,
-                         class_nb, class_nb))
-    val_conf_mat = np.zeros((last_epoch,
-                            class_nb, class_nb))
-    
+    conf_mat = np.zeros((last_epoch, class_nb, class_nb))
+    val_conf_mat = np.zeros((last_epoch, class_nb, class_nb))
+
     for epoch in tqdm(range(start_epoch, last_epoch), desc='epoch'):
         # Train for one epoch
         metrics, sample_win,\
@@ -69,8 +70,8 @@ def train_net(dataloader, model, opt,
                                             verbose=False,
                                             visualize=visualize)
         if visualize:
-            conf_win = viz.plot_mat(conf_mat[epoch], win=conf_win,
-                                    title='train conf mat')
+            conf_win = viz.plot_mat(
+                conf_mat[epoch], win=conf_win, title='train conf mat')
 
         if valid_dataloader is not None:
             # Validation for one epoch
@@ -88,8 +89,8 @@ def train_net(dataloader, model, opt,
                                                         visualize=visualize)
         # Display valid conf mat
         if visualize:
-            val_conf_win = viz.plot_mat(val_conf_mat[epoch], win=val_conf_win,
-                                        title='val conf mat')
+            val_conf_win = viz.plot_mat(
+                val_conf_mat[epoch], win=val_conf_win, title='val conf mat')
 
         # Save network weights
         if opt.save_latest:
@@ -108,29 +109,38 @@ def train_net(dataloader, model, opt,
 
         # Test with aggregation
         if test_aggreg:
-            valid_mean_score = test(valid_dataloader.dataset,
-                                    model, frame_nb=10,
-                                    opt=opt)
+            valid_mean_score = test(
+                valid_dataloader.dataset, model, frame_nb=10, opt=opt)
             valid_mean_scores.append(valid_mean_score)
 
             # Display and save validations info
-            viz.log_errors(epoch=epoch,
-                           errors={'val_aggr_err': valid_mean_score},
-                           log_path=viz.valid_aggreg_log_path)
+            viz.log_errors(
+                epoch=epoch,
+                errors={'val_aggr_err': valid_mean_score},
+                log_path=viz.valid_aggreg_log_path)
             if visualize:
-                valid_mean_win = viz.plot_errors(np.array(list(range(epoch + 1))),
-                                                 np.array(valid_mean_scores),
-                                                 title='average aggreg acc',
-                                                 win=valid_mean_win)
+                valid_mean_win = viz.plot_errors(
+                    np.array(list(range(epoch + 1))),
+                    np.array(valid_mean_scores),
+                    title='average aggreg acc',
+                    win=valid_mean_win)
 
     if verbose:
         print('Done training')
 
 
-def data_pass(model, image, target, opt,
-              dataloader, epoch, i=0, metrics=None,
-              viz=None, conf_mat=None,
-              sample_win=None, train=True,
+def data_pass(model,
+              image,
+              target,
+              opt,
+              dataloader,
+              epoch,
+              i=0,
+              metrics=None,
+              viz=None,
+              conf_mat=None,
+              sample_win=None,
+              train=True,
               visualize=True):
     """
     Args:
@@ -156,25 +166,34 @@ def data_pass(model, image, target, opt,
         pred_classes = output.data.max(1)[1].cpu().numpy()
         target_classes = target.data.max(1)[1].cpu().numpy()
         for idx in range(len(pred_classes)):
-            conf_mat[epoch, target_classes[idx],
-                     pred_classes[idx]] += 1
+            conf_mat[epoch, target_classes[idx], pred_classes[idx]] += 1
 
     # Display an image example in visdom
     if visualize:
         if viz is not None and i % opt.display_freq == 0:
-            sample_win = viz.plot_sample(image.data,
-                                         target.data,
-                                         output.data,
-                                         dataloader.dataset.classes,
-                                         sample_win,
-                                         unnormalize=dataloader.dataset.untransform)
+            sample_win = viz.plot_sample(
+                image.data,
+                target.data,
+                output.data,
+                dataloader.dataset.classes,
+                sample_win,
+                unnormalize=dataloader.dataset.untransform)
 
     return metrics, sample_win, conf_mat
 
 
-def epoch_pass(dataloader, model, opt, epoch, metrics, viz,
-               sample_win=None, train=True, verbose=False,
-               conf_mat=None, conf_win=None, visualize=True):
+def epoch_pass(dataloader,
+               model,
+               opt,
+               epoch,
+               metrics,
+               viz,
+               sample_win=None,
+               train=True,
+               verbose=False,
+               conf_mat=None,
+               conf_win=None,
+               visualize=True):
     if train:
         model.net.train()
     else:
@@ -200,17 +219,18 @@ def epoch_pass(dataloader, model, opt, epoch, metrics, viz,
     for metric in metrics:
         metric.update_epoch()
 
-    last_scores = {metric.name: metric.evolution[-1]
-                   for metric in metrics}
+    last_scores = {metric.name: metric.evolution[-1] for metric in metrics}
 
     # Display scores in visdom
     for metric in metrics:
         plt_title = 'valid ' + metric.name if train is False else metric.name
         scores = np.array(metric.evolution)
         if visualize:
-            win = viz.plot_errors(np.array(list(range(len(scores)))),
-                                  scores, title=plt_title,
-                                  win=metric.win)
+            win = viz.plot_errors(
+                np.array(list(range(len(scores)))),
+                scores,
+                title=plt_title,
+                win=metric.win)
             metric.win = win
 
     # Write scores to log file
@@ -221,9 +241,11 @@ def epoch_pass(dataloader, model, opt, epoch, metrics, viz,
     # while accounting for last batch discrepancy
 
     if last_scores['top1'] != epoch_conf_mat.trace() / epoch_conf_mat.sum():
-        import pdb; pdb.set_trace()
+        import pdb
+        pdb.set_trace()
     assert last_scores['top1'] == epoch_conf_mat.trace() / epoch_conf_mat.sum(
-    ), '{} is not {}'.format(last_scores['top1'], epoch_conf_mat.trace() / epoch_conf_mat.sum())
+    ), '{} is not {}'.format(last_scores['top1'],
+                             epoch_conf_mat.trace() / epoch_conf_mat.sum())
 
     if verbose:
         print(message)
