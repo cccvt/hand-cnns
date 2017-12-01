a1e67192163cee8cce3a4428cb7ac3beebe344ab
Yana Hasson <yana.hasson@inria.fr>
2017-12-01 10:10:49 +0100

    Add safeguard for reusing existing checkpoint directories

--- src/utils/filesys.py
+++ src/utils/filesys.py
@@ -1,5 +1,14 @@
 import os
 
-def mkdir(path):
+
+def mkdir(path, safeguard=True):
     if not os.path.exists(path):
         os.makedirs(path)
+    else:
+        if safeguard:
+            print('\033[93m folder {} already exists! \033[0m'.format(path))
+            answer = input('Do you want to continue [y|n]?')
+            if answer[0].lower() == 'n':
+                raise SystemExit('Exiting according to user instruction')
+            else:
+                print('Continuing with existing directory')
