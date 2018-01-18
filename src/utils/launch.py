ff6f82717b1d977834be3caee91889a0328213e5
Yana Hasson <yana.hasson@inria.fr>
2018-01-18 15:45:00 +0100

    Refactor train and test folder creation functions

--- src/utils/launch.py
+++ src/utils/launch.py
@@ -1,18 +1,25 @@
 import os
 
 
-def get_smthg_paths(network, modality, dataset, experience, split):
+def get_smthg_train(network, modality, dataset, experience):
     train_folder = os.path.join('train', network, modality, dataset,
-                                experience)
-    test_folder = os.path.join('test', network, modality, dataset, experience,
-                               split)
+            experience)
+    return train_folder
+
+def get_smthg_test(network, modality, dataset, experience, split, epoch):
+    test_folder = os.path.join('test', network, modality, dataset, experience, 'epoch_{}'.format(epoch), split)
+    return test_folder
+
+def get_smthg_paths(network, modality, dataset, experience, split, epoch):
+    train_folder = get_smthg_train(network, modality, dataset, experience)
+    test_folder = get_smthg_test(network, modality, dataset, experience, split, epoch)
     return train_folder, test_folder
 
 
 def get_gtea_paths(network, modality, dataset, experience, leave_out):
     leave_out_str = 'gtea_lo_{}'.format(leave_out)
     train_folder = os.path.join('train', network, modality, dataset,
-                                experience, leave_out_str)
+            experience, leave_out_str)
     test_folder = os.path.join('test', network, modality, dataset, experience,
-                               leave_out_str)
+            leave_out_str)
     return train_folder, test_folder
