529e4d1112793882b49437f735da585d232f41b5
Yana Hasson <yanahasson@gmail.com>
2018-02-02 14:29:42 +0100

    Copy first layer weights so as to preserve activation range for i3d

--- src/nets/i3d_adapt.py
+++ src/nets/i3d_adapt.py
@@ -18,11 +18,19 @@ class I3DAdapt(BaseNet):
             use_bn=False)
         if in_channels is not None:
             # Make sure different nb of in_channels is requested
-            if self.net.conv3d_1a_7x7.conv3d.in_channels != in_channels:
+            old_inchannels = self.net.conv3d_1a_7x7.conv3d.in_channels
+            weight_3d = self.net.conv3d_1a_7x7.conv3d.weight.data
+            bias_3d = self.net.conv3d_1a_7x7.conv3d.bias
+            new_weight_3d = weight_3d.mean(1)
+            new_weight_3d = new_weight_3d.unsqueeze(1).repeat(
+                1, in_channels, 1, 1, 1)
+            new_weight_3d = new_weight_3d * old_inchannels / in_channels
+            if old_inchannels != in_channels:
                 conv3d_1a_7x7 = Unit3Dpy(
                     out_channels=64,
                     in_channels=in_channels,
                     kernel_size=(7, 7, 7),
                     stride=(2, 2, 2),
                     padding='SAME')
+                conv3d_1a_7x7.weight = nn.parameter.Parameter(new_weight_3d)
                 self.net.conv3d_1a_7x7 = conv3d_1a_7x7
